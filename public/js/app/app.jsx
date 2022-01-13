cc5751006fd9b5cd1f3d1c9931a9addc6bb87cef
Dag Reuterskiöld <dagreuterskiold@wiksvpn-76-23.remote-access.kth.se>
2022-01-13 10:33:38 +0100

    added routes for secure links'

--- public/js/app/app.jsx
+++ public/js/app/app.jsx
@@ -40,6 +40,18 @@ function appFactory(applicationStore) {
     <MobxStoreProvider initCallback={() => applicationStore}>
       <Switch>
         <Route exact path="/" component={Start} />
+        <Route exact path="/secure">
+          <a href="/node/">Tillbaka</a>
+          <h1>Secured page</h1>
+        </Route>
+        <Route exact path="/secure/admin">
+          <a href="/node/">Tillbaka</a>
+          <h1>Secured Admin page</h1>
+        </Route>
+        <Route exact path="/silent">
+          <a href="/node/">Tillbaka</a>
+          <h1>Silent page</h1>
+        </Route>
       </Switch>
     </MobxStoreProvider>
   )
