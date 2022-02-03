9dbeecadf29ccff7d7934396a49abc99f3f7aba2
Won-Kyung Chung <wkchung@kth.se>
2022-02-03 14:28:46 +0100

    Add .env.ini and check for env variables at app start

--- Dockerfile
+++ Dockerfile
@@ -17,6 +17,7 @@ COPY ["server", "server"]
 COPY ["app.js", "app.js"]
 COPY ["build.sh", "build.sh"]
 COPY ["webpack.config.js", "webpack.config.js"]
+COPY [".env.ini", ".env.ini"]
 
 RUN apk stats && \
     chmod a+rx build.sh && \
--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config()
 
+const fs = require('fs')
 const log = require('@kth/log')
 const config = require('./server/configuration').server
 const server = require('./server/server')
@@ -9,17 +10,51 @@ const server = require('./server/server')
 const packageFile = require('./package.json')
 
 // catches uncaught exceptions
-process.on('uncaughtException', err => {
+process.on('uncaughtException', (err, origin) => {
   log.error('APPLICATION EXIT - uncaught exception in ', packageFile.name)
-  log.error('Uncaught Exception', { err })
+  log.error(`Uncaught Exception, origin (${origin})`, { err })
   process.exit(1)
 })
+// catches unhandled promise rejections
+process.on('unhandledRejection', reason => {
+  // This line below provokes an uncaughtException and will be caught few lines
+  // above
+  log.error(`unhandledRejection  ${packageFile.name}`, reason)
+  // throw reason
+})
+
+function checkEnvironment() {
+  try {
+    log.info(`Checking environment variables from .env.ini file.`)
+    const lines = fs
+      .readFileSync('./.env.ini')
+      .toString()
+      .split('\n')
+      .filter(l => l.trim() && !l.startsWith('#'))
+      .filter(l => l)
+
+    const isDevelopment = process.env.NODE_ENV !== 'production'
+    lines.forEach(l => {
+      const name = l.substring(0, l.indexOf('=')).trim()
+      if (name && process.env[name] !== undefined) {
+        log.debug(`   Environment variable '${name}' found`)
+      } else if (isDevelopment) {
+        log.debug(`   Environment variable '${name}' is missing, most likely there is a default value.`)
+      } else {
+        log.warn(`   Environment variable '${name}' is missing.`)
+      }
+    })
+    log.info(`Checking environment variables completed.`)
+  } catch (err) {
+    log.error({ err }, `Failed to check environment variables`)
+  }
+}
+checkEnvironment()
 
 /* ****************************
  * ******* SERVER START *******
  * ****************************
  */
-// Exports a promise to use in integration tests
 module.exports = server.start({
   useSsl: config.useSsl,
   pfx: config.ssl.pfx,
