fee4a675441ceaa90923762347433fbb6b84a07b
Rickard Falk <ricf@kth.se>
2024-07-12 11:38:02 +0200

    add user form on AdminStart page with useTransition

--- public/js/app/pages/AdminStart.jsx
+++ public/js/app/pages/AdminStart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useTransition } from 'react'
 
 import i18n from '../../../../i18n'
 
@@ -23,6 +23,23 @@ const AdminStart = () => {
   }
   const [alertConfig, setAlertConfig] = useState(initAlertConfig)
 
+  const [id, setId] = useState('')
+  const [name, setName] = useState('')
+  const [isPending, startTransition] = useTransition()
+
+  const postData = async () => null // ({ message: 'Äsch' })
+
+  const handleSubmit = () => {
+    startTransition(async () => {
+      const error = await postData(id, name)
+      if (error) {
+        setAlertConfig({ type: 'warning', rawMessage: error.message })
+        return
+      }
+      setAlertConfig({ type: 'info', rawMessage: `Successfully updated name with ${id} and ${name}` })
+    })
+  }
+
   return (
     <>
       <nav id="mainMenu" className="col navbar navbar-expand-lg navbar-light sticky-menu" aria-label="Sub menu">
@@ -33,9 +50,41 @@ const AdminStart = () => {
         <a href="/node/">{i18n.message('template_back_link', lang)}</a>
       </nav>
       <MainContent>
-        <h1>Node-web Admin page</h1>
+        <h1>{i18n.message('heading_admin_page', lang)}</h1>
         <h2>{i18n.message('template_app_works', lang)}</h2>
         <Alert config={alertConfig} setConfig={setAlertConfig} />
+        <form>
+          <h3>{i18n.message('heading_add_new_user', lang)}</h3>
+          <div className="form-group">
+            <label className="form-control-label" htmlFor="id">
+              {i18n.message('field_label_id', lang)}
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="id"
+              value={id}
+              onChange={event => setId(event.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label className="form-control-label" htmlFor="name">
+              {i18n.message('field_label_name', lang)}
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              value={name}
+              onChange={event => setName(event.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <button type="button" className="kth-button primary" onClick={handleSubmit} disabled={isPending}>
+              {i18n.message('button_label_add', lang)}
+            </button>
+          </div>
+        </form>
       </MainContent>
     </>
   )
