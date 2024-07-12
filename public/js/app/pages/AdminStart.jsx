c023618dd983008127d077881b8a0df3bb2cecd9
Rickard Falk <ricf@kth.se>
2024-07-12 10:40:33 +0200

    use Alert component on AdminStart page

--- public/js/app/pages/AdminStart.jsx
+++ public/js/app/pages/AdminStart.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import i18n from '../../../../i18n'
 
@@ -6,6 +6,7 @@ import { useWebContext } from '../context/WebContext'
 import { useAdminContext } from '../context/AdminContext'
 
 import MainContent from '../components/MainContent'
+import Alert from '../components/Alert'
 
 const AdminStart = () => {
   const [webContext] = useWebContext()
@@ -13,6 +14,14 @@ const AdminStart = () => {
 
   const [adminContext] = useAdminContext()
   const { adminData = {} } = adminContext
+
+  const initAlertConfig = {
+    type: 'info',
+    rawMessage: `Message in WebContext: ${message} – Data in AdminContext: (x: ${adminData.x}, y: ${adminData.y})`,
+    timeout: 5000,
+    lang,
+  }
+  const [alertConfig, setAlertConfig] = useState(initAlertConfig)
 
   return (
     <>
@@ -26,11 +35,7 @@ const AdminStart = () => {
       <MainContent>
         <h1>Node-web Admin page</h1>
         <h2>{i18n.message('template_app_works', lang)}</h2>
-        <div className="kth-alert info" role="alert">
-          {`Message in WebContext: ${message}`}
-          <br />
-          {`Data in AdminContext: (x: ${adminData.x}, y: ${adminData.y})`}
-        </div>
+        <Alert config={alertConfig} setConfig={setAlertConfig} />
       </MainContent>
     </>
   )
